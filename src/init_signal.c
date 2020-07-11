684d4c143a2f7a60f728755685b9abf2aade78c2
i-minakov <ilya0305@gmail.com>
2020-07-11 11:35:01 +0300

    lala

--- src/init_signal.c
+++ src/init_signal.c
@@ -67,14 +67,11 @@ void init_signals(t_main *m) {
     g_signal_connect(m->but1, "clicked", G_CALLBACK(send_but), m);
     g_signal_connect(m->sms, "activate", G_CALLBACK(entry_activate), m);
     g_signal_connect(m->sms, "icon-press", G_CALLBACK(attach_file), m);
-<<<<<<< HEAD
     g_signal_connect(m->search, "activate", G_CALLBACK(search_msg), m);
     g_signal_connect(m->scrol_bar, "edge-reached", G_CALLBACK(fun), m);
-=======
 
     g_signal_connect(m->menu->settings, "clicked", G_CALLBACK(show_setings), m);
     //g_signal_connect(m->menu->contacts, "clicked", G_CALLBACK(send_but), m);
     //g_signal_connect(m->exit, "clicked", G_CALLBACK(send_but), m);
     g_signal_connect(m->set->set_but, "clicked", G_CALLBACK(hide_setings), m);
->>>>>>> 07d398e9ae83888db01a1730699de159d91143aa
 }
--- src/switch_menu.c
+++ src/switch_menu.c
@@ -1,63 +1,23 @@
 #include "../inc/uchat.h"
 
-<<<<<<< HEAD
-static void hide_me(t_main *m) {
-    gtk_widget_hide(m->menu->menu_box);
-    gtk_widget_hide(m->menu->menu_img);
-    gtk_widget_hide(m->menu->lang_text);
-    gtk_widget_hide(m->menu->color_text);
-    gtk_widget_hide(m->menu->lang1);
-    gtk_widget_hide(m->menu->lang2);
-    gtk_widget_hide(m->menu->color1);
-    gtk_widget_hide(m->menu->color2);
-    gtk_widget_hide(m->menu->user_search);
-    m->menu->flag = 0;
-}
-
-static void show_me(t_main *m) {
-    gtk_widget_show(m->menu->menu_box);
-    gtk_widget_show(m->menu->menu_img);
-    gtk_widget_show(m->menu->lang_text);
-    gtk_widget_show(m->menu->color_text);
-    gtk_widget_show(m->menu->lang1);
-    gtk_widget_show(m->menu->lang2);
-    gtk_widget_show(m->menu->color1);
-    gtk_widget_show(m->menu->color2);
-    gtk_widget_show(m->menu->user_search);
-    m->menu->flag = 1;  
-}
-
-=======
->>>>>>> 07d398e9ae83888db01a1730699de159d91143aa
 void switch_menu(GtkWidget *wid, t_main *m) {
     char *way = NULL;
     int y;
 
-<<<<<<< HEAD
-    if (m->menu->flag == 0) {
-        way = "./src/resource/cancel.png";
-        y = 42;
-        show_me(m);
-=======
     if (m->menu->flag == 0 && m->set->flag == 0 ) { 
         way = "./src/resource/cancel.png";
         y = 42;
         show_menu(m);
->>>>>>> 07d398e9ae83888db01a1730699de159d91143aa
     }
     else {
         way = "./src/resource/burger2.png";
         y = 37;
-<<<<<<< HEAD
-        hide_me(m);
-=======
         if (m->set->flag == 1)
             hide_set(m);
         // if (m->cont->flag == 1)
         //     hide_cont(m);
         else
             hide_menu(m);
->>>>>>> 07d398e9ae83888db01a1730699de159d91143aa
     }
     gtk_widget_destroy(m->cap->burger_but_img);
     m->cap->burger_but_img = gtk_image_new_from_file(way);
