4652b7c95fa86c15e846f853fd294494b0f61de0
i-minakov <ilya0305@gmail.com>
2020-07-11 19:31:20 +0300

    merger

--- src/main.c
+++ src/main.c
@@ -138,7 +138,7 @@ int main(int argc, char *argv[]) {
     m->set = (t_setting *)malloc(sizeof(t_setting) * 100);
     m->users = NULL;
 
-    log_screen();
+    // log_screen();
     
     for (int i = atoi(argv[1]); i > 0; i--)
         user_pushback(&m->users);
