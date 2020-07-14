2d061c4b1b8c81df11bfa32b6c3869f273574272
hlgltvnnk <olalytvinenko@gmail.com>
2020-07-14 22:06:12 +0300

    olya

--- src/show_menu.c
+++ src/show_menu.c
@@ -1,10 +1,11 @@
 #include "../inc/uchat.h"
 
 void show_menu(t_main *m) {
-    gtk_widget_show(m->menu->menu_box);
-    gtk_widget_show(m->menu->search);
-    gtk_widget_show(m->menu->settings);
-    gtk_widget_show(m->menu->contacts);
-    gtk_widget_show(m->menu->exit);
+    gtk_widget_show(m->menu->menu_fix);
+    // gtk_widget_show(m->menu->menu_box);
+    // gtk_widget_show(m->menu->search);
+    // gtk_widget_show(m->menu->settings);
+    // gtk_widget_show(m->menu->contacts);
+    // gtk_widget_show(m->menu->exit);
     m->menu->flag = 1;
 }
