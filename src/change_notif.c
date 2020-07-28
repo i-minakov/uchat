21a4848ae0c10f094a7b24a88e92656a750bc464
yohrudkov <68469253+yohrudkov@users.noreply.github.com>
2020-07-28 12:36:57 +0300

    lol

--- src/change_notif.c
+++ src/change_notif.c
@@ -1,6 +1,7 @@
 #include "../inc/uchat.h"
 
 void change_notif(GtkToggleButton *togglebutton, t_main *m) {
+    (void)togglebutton;
     if(m->set->notif_flag == 1)
         m->set->notif_flag = 0;
     else 
